5a159b48341f8465d831108a72bb26081929b646
Rishuuprajapati0722 <riteshprajapti1234@gmail.com>
2024-07-26 12:50:11 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -2,7 +2,56 @@ const numIslands = function (grid) {
 
   
 
-  
+  if (!grid || grid.length === 0) return 0;
+
+  let numIslands = 0;
+
+  function dfs(grid, i, j) {
+      if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] === 'W') {
+          return;
+      }
+      
+      // Mark the land as visited
+      grid[i][j] = 'W';
+
+      // Explore all four directions
+      dfs(grid, i + 1, j);
+      dfs(grid, i - 1, j);
+      dfs(grid, i, j + 1);
+      dfs(grid, i, j - 1);
+  }
+
+  for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[0].length; j++) {
+          if (grid[i][j] === 'L') {
+              numIslands++;
+              dfs(grid, i, j);
+          }
+      }
+  }
+
+  return numIslands;
+}
+
+// Test cases
+const map1 = [
+  ["L","L","L","L","W"],
+  ["L","L","W","L","W"],
+  ["L","L","W","W","W"],
+  ["W","W","W","W","W"]
+];
+
+const map2 = [
+  ["L","L","W","W","W"],
+  ["L","L","W","W","W"],
+  ["W","W","L","W","W"],
+  ["W","W","W","L","L"]
+];
+
+console.log(numIslands(map1));  
+console.log(numIslands(map2));  
+
+
   // write your code here
 
 };
