da7653c2403d2e309c970446e1b1d799b839da2d
Rishuuprajapati0722 <riteshprajapti1234@gmail.com>
2024-07-26 13:40:14 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,45 +1,34 @@
-// const decodeTheRing = function (s, p) {
+Here is the JavaScript program that solves the Secret Decoder Ring Challenge:
 
-//     // write your code here
 
-//   };
-  
-//   module.exports = decodeTheRing;
+function decodeMessage(message, decoderKey) {
+    let messageIndex = 0;
+    let decoderKeyIndex = 0;
 
-function isMatch(message, pattern) {
-  const m = message.length;
-  const p = pattern.length;
-  
-  // Create a 2D array dp where dp[i][j] means whether message[0...i-1] matches pattern[0...j-1]
-  const dp = Array(m + 1).fill(false).map(() => Array(p + 1).fill(false));
-  
-  // Base case: Empty pattern matches empty message
-  dp[0][0] = true;
-  
-  // Fill the first row for patterns with *
-  for (let j = 1; j <= p; j++) {
-      if (pattern[j - 1] === '*') {
-          dp[0][j] = dp[0][j - 1];
-      }
-  }
-  
-  // Fill the dp table
-  for (let i = 1; i <= m; i++) {
-      for (let j = 1; j <= p; j++) {
-          if (pattern[j - 1] === '*') {
-              dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
-          } else if (pattern[j - 1] === '?' || pattern[j - 1] === message[i - 1]) {
-              dp[i][j] = dp[i - 1][j - 1];
-          }
-      }
-  }
-  
-  return dp[m][p];
+    while (messageIndex < message.length && decoderKeyIndex < decoderKey.length) {
+        if (decoderKey[decoderKeyIndex] === '*') {
+            decoderKeyIndex++;
+            while (messageIndex < message.length && (decoderKeyIndex === decoderKey.length || message[messageIndex] !== decoderKey[decoderKeyIndex])) {
+                messageIndex++;
+            }
+        } else if (decoderKey[decoderKeyIndex] === '?') {
+            messageIndex++;
+            decoderKeyIndex++;
+        } else if (message[messageIndex] === decoderKey[decoderKeyIndex]) {
+            messageIndex++;
+            decoderKeyIndex++;
+        } else {
+            return false;
+        }
+    }
+
+    return messageIndex === message.length && decoderKeyIndex === decoderKey.length;
 }
 
 // Test cases
-console.log(isMatch("aa", "a")); // Output: false
-console.log(isMatch("aa", "*")); // Output: true
-console.log(isMatch("cb", "?a")); // Output: false
-console.log(isMatch("adceb", "*a*b")); // Output: true
-console.log(isMatch("acdcb", "a*c?b")); // Output: false
+console.log(decodeMessage("aa", "a"));  // false
+console.log(decodeMessage("aa", "*"));  // true
+console.log(decodeMessage("cb", "?a"));  // false
+
+
+This JavaScript program works similarly to the Python program. It uses two pointers, `
